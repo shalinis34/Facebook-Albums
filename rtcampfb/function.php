a1f75e3196490b35448b8e826ae6818d0a88c5a3
shalinis34 <singhshalinis34@gmail.com>
2015-10-08 00:21:55 +0530

    final change

--- rtcampfb/function.php
+++ rtcampfb/function.php
@@ -1,4 +1,5 @@
 <?php
+require_once 'config.php';
 /* 
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
--- rtcampfb/lib/move.php
+++ rtcampfb/lib/move.php
@@ -1,29 +1,11 @@
 <?php
-/**
- * Zend Framework
- *
- * LICENSE
- *
- * This source file is subject to the new BSD license that is bundled
- * with this package in the file LICENSE.txt.
- * It is also available through the world-wide-web at this URL:
- * http://framework.zend.com/license/new-bsd
- * If you did not receive a copy of the license and are unable to
- * obtain it through the world-wide-web, please send an email
- * to license@zend.com so we can send you a copy immediately.
- *
- * @category   Zend
- * @package    Zend_Gdata
- * @subpackage Demos
- * @copyright  Copyright (c) 2005-2012 Zend Technologies USA Inc. (http://www.zend.com)
- * @license    http://framework.zend.com/license/new-bsd     New BSD License
- */
-
+session_start();
+require_once '../config.php';
+require_once '../function.php';
 /**
  * Utilizes the Zend Framework Gdata components to communicate with the Google API.
  * Requires the Zend Framework Gdata components and PHP >= 5.2.11
  */
-
 require_once 'Zend/Loader.php';
 Zend_Loader::loadClass('Zend_Gdata');
 Zend_Loader::loadClass('Zend_Gdata_AuthSub');
@@ -42,46 +24,37 @@ Zend_Loader::loadClass('Zend_Gdata_App_Extension_Category');
  * @param  array            $photo   The uploaded photo
  * @return void
  */
-function addPhoto($client, $user, $albumId, $photo_name,$photo_path)
-{
+function addPhoto($client, $user, $albumId, $photo_name, $photo_path) {
     $photos = new Zend_Gdata_Photos($client);
     $fd = $photos->newMediaFileSource($photo_path);
     $fd->setContentType('image/jpeg');
     $entry = new Zend_Gdata_Photos_PhotoEntry();
     $entry->setMediaSource($fd);
     $entry->setTitle($photos->newTitle($photo_name));
-
     $albumQuery = new Zend_Gdata_Photos_AlbumQuery;
     $albumQuery->setUser($user);
     $albumQuery->setAlbumId($albumId);
-
     $albumEntry = $photos->getAlbumEntry($albumQuery);
-
     $result = $photos->insertPhotoEntry($entry, $albumEntry);
     if ($result) {
         return $result;
-       // print_r($result);
     } else {
         echo "There was an issue with the file upload.";
     }
 }
 
-
 /**
  * Adds a new album to the specified user's album
  *
  * @param  Zend_Http_Client $client The authenticated client
- * @param  string           $user   The user's account name
  * @param  string           $name   The name of the new album
  * @return void
  */
-function addAlbum($client, $user, $name)
-{
+function addAlbum($client,$name) {
     $photos = new Zend_Gdata_Photos($client);
 
     $entry = new Zend_Gdata_Photos_AlbumEntry();
     $entry->setTitle($photos->newTitle($name));
-
     $result = $photos->insertAlbumEntry($entry);
     if ($result) {
         return $result;
@@ -90,3 +63,34 @@ function addAlbum($client, $user, $name)
     }
 }
 
+/* * **********************************************
+  If we're signed in we can go ahead and retrieve
+  data
+ * ********************************************** */
+set_time_limit('10000');
+if (isset($_SESSION['google_token'])) {
+    $client = Zend_Gdata_AuthSub::getHttpClient($_SESSION['google_token']);
+    $userInfo = $_SESSION['google_userInfo'];
+    $album_ids = explode(',', $_GET['album_ids']);
+    foreach ($album_ids as $album_id) {
+        $singleAlbum = fetchSingleAlbums($album_id);
+        if (!file_exists(ABSOLUTE_PATH . "/assets/images/archive")) {
+            mkdir(ABSOLUTE_PATH . "/assets/images/archive");
+        }
+        $filepath = $album_id;
+        if (!file_exists(ABSOLUTE_PATH . "/assets/images/albums/$filepath")) {
+            mkdir(ABSOLUTE_PATH . "/assets/images/albums/$filepath");
+        }
+        $result = addAlbum($client,'Test Album');
+        $albumId = $result->getGphotoId()->getText();
+        foreach ($singleAlbum as $photo) {
+            $url = $photo->source;
+            $destination_path = ABSOLUTE_PATH . '/assets/images/albums/' . $filepath . '/' . $photo->id . '.jpg';
+            downloadImage($url, $destination_path);
+            addPhoto($client, $userInfo->user_id, $albumId, $photo->id, $destination_path);
+        }
+    }
+      echo '<p class="bg-success"> Your album has been move to your picasa account successfully.</p>';
+} else {
+    echo 'first login via google inorder to move your album to picasa account';
+}
