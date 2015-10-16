74859bcff3365fa6ade10cd4d1a4fb6918ec3d56
shalinis34 <singhshalinis34@gmail.com>
2015-10-17 00:10:46 +0530

    unlink directory and issue fixes

--- rtcampfb/logout.php
+++ rtcampfb/logout.php
@@ -1,5 +1,11 @@
 <?php
 require_once 'config.php';
+require_once 'function.php';
 session_start();
 session_destroy();
+$files = glob(ABSOLUTE_PATH . '/assets/images/archive/*'); // get all file names
+foreach($files as $file){ // iterate files
+  if(is_file($file))
+    unlink($file); // delete file
+}
 header('Location:'.BASE_URL);
